4eb506bcbafe150f2ae052d595c9c12b747b1fcb
Justin Schilling <justin.schilling24@gmail.com>
2022-03-10 10:41:13 +0100

    certbot

--- main.go
+++ main.go
@@ -2,9 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"time"
 )
+
+func redirect(w http.ResponseWriter, r *http.Request) {
+	// remove/add not default ports from req.Host
+	target := "https://" + r.Host + r.URL.Path
+	if len(r.URL.RawQuery) > 0 {
+		target += "?" + r.URL.RawQuery
+	}
+	log.Printf("redirect to: %s", target)
+	http.Redirect(w, r, target, http.StatusTemporaryRedirect)
+}
 
 func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World! %s", time.Now())
@@ -12,5 +23,5 @@ func greet(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":80", nil)
 }
